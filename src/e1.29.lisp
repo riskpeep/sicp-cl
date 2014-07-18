19ca1c360937078f0c8a9d199549e90b7d8f32b1
Robert Kerr <rskerr@gmail.com>
2014-07-18 11:11:46 -0400

    Started work on solution for e1.29

--- src/e1.29.lisp
+++ src/e1.29.lisp
@@ -5,7 +5,7 @@
 ; function f between a and b is approximated as
 ; 
 ;  h
-; --- (y0 + 4 * y1 + 2 * y2 + 4 * y3 + 2 * y4 +  ... + 2 * yn-2 + 4 * yn-1 + yn),
+; --- (y₀ + 4 * y₁ + 2 * y₂ + 4 * y₃ + 2 * y₄ +  ... + 2 * yn-2 + 4 * yn-1 + yn),
 ;  3
 ;
 ; where
@@ -22,3 +22,25 @@
 ; value of the integral, computed using Simpson’s Rule. Use your procedure
 ; to integrate cube between 0 and 1 (with n = 100 and n = 1000), and 
 ; compare the results to those of the integral procedure shown above.
+
+;; We begin by defining some utility functions for h and yk.
+(defun h-term (a b n)
+  (/ (- b a) n))
+
+;; TODO need to figure out the syntax to pass in a function here
+(defun yk-term (f a k h)
+  (#'f (+ a (* k h))))
+
+;; TODO notice that the first and last terms are not multiplied.  Need to make that work
+(defun simpsons-rule-integral (f a b n)
+  (defun simpsons-iterator (f a k n current-term)
+    (cond ((= current-term n) nil)
+          (T (+ (yk-term f a k n (1+ current-term))
+                (yk-term f a k n current-term)
+                (simpsons-iterator f a k n (+ 2 current-term))
+                ))
+          )
+    )
+  (cond ((not (evenp n))
+         nil
+         )))
