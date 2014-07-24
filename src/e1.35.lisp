dc27a217769392586fb0b38255c01a19f06acab9
Robert Kerr <rskerr@gmail.com>
2014-07-24 10:41:22 -0400

    Added definitions for e1.35 - e1.37

---
+++ src/e1.35.lisp
@@ -0,0 +1,10 @@
+;Exercise 1.35:
+;
+;  Show that the golden ratio ϕ (Section 1.2.2) is a fixed point of the 
+;  transformation
+;
+;           1
+;  x → 1 + ---, 
+;           x
+;
+;  and use this fact to compute ϕ by means of the fixed-point procedure.
---
+++ src/e1.37.lisp
@@ -0,0 +1,47 @@
+;Exercise 1.37:
+;
+;  a. An infinite continued fraction is an expression of the form
+;
+;                  N₁
+;    f = ----------------------
+;                     N₂
+;         D₁ + ----------------
+;                        N₃
+;               D₂ + ----------
+;                     D₃ + ...
+;
+;  As an example, one can show that the infinite continued fraction expansion 
+;  with the Ni and the Di all equal to 1 produces 1=ϕ, where ϕ is the golden
+;  ratio (described in Section 1.2.2). One way to approximate an infinite 
+;  continued fraction is to truncate the expansion after a given number of 
+;  terms. Such a truncation — a so-called k-term finite continued fraction
+;  — has the form
+;
+;              N₁
+;    ----------------------
+;                 N₂
+;     D₁ + ----------------
+;                    Nk
+;           ...  ----------
+;                    Dk
+;
+;  Suppose that n and d are procedures of one argument (the term index i) that
+;  return the Ni and Di of the terms of the continued fraction. Define a 
+;  procedure cont-frac such that evaluating
+;
+;  (cont-frac n d k)
+;
+;  computes the value of the k-term finite continued fraction.
+;
+;  Check your procedure by approximating 1=ϕ using
+;
+;  (cont-frac (lambda (i) 1.0)
+;             (lambda (i) 1.0)
+;             k)
+;
+;  for successive values of k. How large must you make k in order to get an 
+;  approximation that is accurate to 4 decimal places?
+;
+;  b. If your cont-frac procedure generates a recursive process, write one 
+;  that generates an iterative process. If it generates an iterative process,
+;  write one that generates a recursive process.
