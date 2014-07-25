ba28f4de341cc196fc997ae84ca372af0c0a3c44
Robert Kerr <rskerr@gmail.com>
2014-07-25 09:02:01 -0400

    Completed e1.35

--- src/e1.35.lisp
+++ src/e1.35.lisp
@@ -12,7 +12,7 @@
 ;; From the text (sec 1.2.2), we have
 ;; 
 ;;      1 + sqrt(5)
-;; ϕ = -------------, and
+;; ϕ = ------------- ~= 1.6180, and
 ;;          2
 ;;
 ;; ϕ² = ϕ + 1
@@ -37,3 +37,37 @@
 ;; The above proof shows that ϕ = 1 + (1 / ϕ).
 ;; 
 ;; Next we compute ϕ using the fixed-point procedure.
+;; 
+;; From the text (sec 1.3.3) we have a definition for fixed-point
+;;
+;; (define tolerance 0.00001)
+;; (define (fixed-point f first-guess)
+;;   (define (close-enough? v1 v2)
+;;     (< (abs (- v1 v2))
+;;        tolerance))
+;;   (define (try guess)
+;;     (let ((next (f guess)))
+;;       (if (close-enough? guess next)
+;;           next
+;;           (try next))))
+;;   (try first-guess))
+;;
+;; Re-writing in CL yields
+;; Here we introduce defparameter which dynamically binds the given name to
+;; the indicated value
+(defparameter tolerance 0.00001)
+(defun fixed-point (f first-guess)
+  (labels ((close-enough? (v1 v2)
+             (< (abs (- v1 v2))
+                tolerance))
+           (try (guess)
+             (let ((next (funcall f guess)))
+               (if (close-enough? guess next)
+                 next
+                 (try next)))))
+    (try first-guess)))
+
+;; Finally, we test to show that it works.  We'll use lambda notation to 
+;; define our function, and use 1.0 as our initial guess
+(= 1.6180328  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+                           1.0))
