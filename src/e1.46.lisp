a88216a23579c14b3b0e5145ed1954c3e84edfee
Robert Kerr <rskerr@gmail.com>
2014-07-31 03:08:09 -0400

    Working on e1.46

--- src/e1.46.lisp
+++ src/e1.46.lisp
@@ -12,3 +12,45 @@
 ;  the guess until it is good enough. Rewrite the sqrt procedure of 
 ;  Section 1.1.7 and the fixed-point procedure of Section 1.3.3 in terms of
 ;  iterative-improve.
+
+;; We begin by writing iterative-improve
+(defun iterative-improve (good-enoughp improve-guess)
+  (labels ((iteration (guess x) (if (funcall good-enoughp guess x)
+                                     guess
+                                     (iteration (funcall improve-guess guess x) x))))
+    (lambda (x) (iteration (funcall improve-guess 1.0 x) x))))
+
+;; To implement sqrt, we need first to define the procedures for good-enough 
+;; and improve-guesss.  From 1.1.7, we have
+;; 
+;; (define (average x y)
+;;   (/ (+ x y) 2))
+;;
+;; (define (improve guess x)
+;;   (average guess (/ x guess))) 
+;;
+;; (define (good-enough? guess x)
+;;   (< (abs (- (square guess) x)) 0.001))
+;;
+;; In CL we get
+(defun square (x)
+  (* x x))
+
+(defun average (x y)
+  (/ (+ x y) 2))
+
+(defun improve (guess x)
+  (average guess (/ x guess))) 
+
+(defun good-enoughp (guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+;; Now we can implment sqrt
+(defun im-sqrt (x)
+  (funcall (iterative-improve #'good-enoughp #'improve) x))
+
+;; Finally we test our sqrt procedure
+;; sqrt of 5.0 = 2.23068
+(= 2.2360687 (im-sqrt 5.0))
+
+
