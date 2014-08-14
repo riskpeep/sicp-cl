d3ace97b0a1806479ebdd7faa48e84372c44090c
Robert Kerr <rskerr@gmail.com>
2014-08-14 11:11:29 -0400

    Completed e2.19

--- src/e2.19.lisp
+++ src/e2.19.lisp
@@ -8,7 +8,7 @@
 ;  count-change (which knows that there are five kinds of U.S. coins). It
 ;  would be nicer to be able to supply a list of coins to be used for making
 ;  change.
-;  
+;
 ;  We want to rewrite the procedure cc so that its second argument is a list
 ;  of the values of the coins to use rather than an integer specifying which
 ;  coins to use. We could then have lists that defined each kind of currency:
@@ -24,7 +24,7 @@
 ;  To do this will require changing the program cc somewhat.  It will still
 ;  have the same form, but it will access its second argument differently, as
 ;  follows:
-;  
+;
 ;  (define (cc amount coin-values)
 ;    (cond ((= amount 0) 1)
 ;          ((or (< amount 0) (no-more? coin-values)) 0)
@@ -40,3 +40,61 @@
 ;  Define the procedures first-denomination, except-first-de-nomination, and
 ;  no-more? in terms of primitive operations on list structures. Does the order
 ;  of the list coinvalues affect the answer produced by cc? Why or why not?
+
+;; To begin, we define first-denomination, except-first-denomination, and 
+;; no-more
+(defun first-denomination (coin-values)
+  (car coin-values))
+
+(defun except-first-denomination (coin-values)
+  (cdr coin-values))
+
+(defun no-more? (coin-values)
+  (if (eq nil coin-values)
+    T
+    nil))
+
+;; Next, we re-write cc in CL
+(defun cc (amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (T
+         (+ (cc amount
+                (except-first-denomination
+                  coin-values))
+            (cc (- amount
+                   (first-denomination
+                     coin-values))
+                coin-values)))))
+
+;; Now we can do some testing
+(defvar us-coins (list 50 25 10 5 1))
+(defvar uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(= 292 (cc 100 us-coins))
+(= 292 (cc 104 us-coins))
+(= 335 (cc 105 us-coins))
+(= 104561 (cc 100 uk-coins))
+
+;; And we can also look at if the order matters
+(defvar us-coins-alt (list 1 5 10 25 50))
+(= 292 (cc 100 us-coins-alt))
+(= 292 (cc 104 us-coins-alt))
+(= 335 (cc 105 us-coins-alt))
+
+;; From these examples, we can see that the order of the denominations does 
+;; not matter.  Understanding why is more interesting.
+;; 
+;; In both cases, the procedure identifies a change counting method by creating
+;; a recursive search tree.  An example of the search tree can be seen in 
+;; e1.11.  Now, the way that the given cc procedure works is to recursively
+;; call itself with fewer coins and with a smaller amount to count change on.
+;; It then adds the sum of the two recursions.  These two branches explore
+;; different sides of the tree through the search space.  In the descending
+;; currency order case, the tree is explored from the opposite direction as
+;; the ascending currency order case.  For example, from e1.11 we see that one
+;; side of the tree is the case of only pennies (or 0.5 in the case of uk 
+;; coins).  This side of the tree is recursed first in the ascending currency
+;; order case.  In the descending currency order case, this branch is recursed
+;; last.
+
