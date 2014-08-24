8c010f7798887b68f94d694dbbbbe62d58e464d5
Robert Kerr <rskerr@gmail.com>
2014-08-24 17:36:37 -0400

    Completed e2.28

--- src/e2.28.lisp
+++ src/e2.28.lisp
@@ -11,29 +11,32 @@
 ;  (1 2 3 4 1 2 3 4)
 
 ;; The challenge in this exercise is to walk the tree, and when we find a car 
-;; or cdr that is not a subtree, then to add the item to the result.  To
-;; achieve left to right order, ...?
-
-(defun iter2 (tree result)
-             (if (null tree)
-               result
-               (cons (if (consp (car tree))
-                       (iter2 (car tree) result)
-                       (car tree))
-                     (iter2 (cdr tree) result)))
-  
-  )
+;; or cdr that is not a subtree, then to add the item to the result.
 (defun fringe (tree)
   (labels ((iter (tree result)
-             (if (null tree)
-               result
-               (cons (if (consp (car tree))
-                       (iter (car tree) result)
-                       (car tree))
-                     (iter (cdr tree) result)))))
-  (iter2 tree nil)))
+             (cond ((null tree) result)
+                   ((not (consp tree)) (cons tree result))
+                   (T (iter (car tree) 
+                            (iter (cdr tree) result))))))  
+
+    (iter tree nil)))
+
 
 ;; Testing
 (defparameter x (list (list 1 2) (list 3 4)))
 
 (fringe x)
+;; (1 2 3 4)
+
+(fringe (list x x))
+;; (1 2 3 4 1 2 3 4)
+
+;; This exercise proved to be particularly challenging for me.  The key mental
+;; leap is that the action in this case needs to occur when we find a leaf.
+;; Then and only then, do we need to add the leaf node to the result.  
+;; Separating this concept from the tree walking iteration concept took me a
+;; number of attempts.  Reviewing section 2.2.2 in the text showed the count-
+;; leaves procedure.  Using the structure of that method (with cond, and not
+;; if then) is what finally enabled me to get this one correct.  Cond is
+;; extrememly useful since it easily lets you have multiple branches in your
+;; procedure and clearly separates the actions taken in each.
