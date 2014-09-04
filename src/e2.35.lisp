21f1d16eee46c187016bc643cd2185f22afde746
Robert Kerr <rskerr@gmail.com>
2014-09-04 09:04:25 -0400

    Started work on e2.35

--- src/e2.35.lisp
+++ src/e2.35.lisp
@@ -4,3 +4,30 @@
 ;
 ;  (define (count-leaves t)
 ;    (accumulate <??> <??> (map <??> <??>)))
+
+;; Before we begin, we bring in the definition for accumulate
+(defun accumulate (op initial sequence)
+  (if (null sequence)
+    initial
+    (funcall op (car sequence)
+                (accumulate op initial (cdr sequence)))))
+
+;; From the definition of accumulate, we see that accumulate requires three
+;; parameters, op, initial, and sequence.  Since we're counting leaves, we
+;; would expect op to be an increment type operator, and initial to be zero
+;; to start.  With that understanding, we would need a map function that
+;; returns a list of the leaves that can be counted when given a tree.  Based
+;; on that approach, we proceed as follows:
+(defun count-leaves (l)
+  (accumulate (lambda (x y) (+ 1 y))
+              0
+              (map 'list
+                   (let ((leaves (lambda (x)
+                                   (cond ((null x) nil)
+                                         ((atom x) x)
+                                         (T (concatenate (funcall leaves (car x))
+                                                         (funcall leaves (cdr x))))))))
+                     (funcall leaves x))
+                   l)))
+
+
