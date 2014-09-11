79531bd532f31ce31a8254dd744058fcce0513d7
Robert Kerr <rskerr@gmail.com>
2014-09-11 16:12:08 -0400

    Started e2.39

--- src/e2.39.lisp
+++ src/e2.39.lisp
@@ -7,3 +7,33 @@
 ;    (fold-right (lambda (x y) <??>) nil sequence))
 ;  (define (reverse sequence)
 ;    (fold-left (lambda (x y) <??>) nil sequence))
+
+;; First we bring in fold-left and fold-right from e2.38
+(defun fold-left (op initial sequence)
+  (labels ((iter (result rest)
+                (if (null rest)
+                  result
+                  (iter (funcall op result (car rest))
+                        (cdr rest)))))
+    (iter initial sequence)))
+
+(defun accumulate (op initial sequence)
+  (if (null sequence)
+    initial
+    (funcall op (car sequence)
+                (accumulate op initial (cdr sequence)))))
+
+(defun fold-right (op initial sequence)
+  (accumulate op initial sequence))
+
+;; Now may begin
+(defun my-reverse (sequence)
+  (fold-right (lambda (x y) (cons x y)) nil sequence))
+
+(defun my-reverse2 (sequence)
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+;; Testing
+(my-reverse (list 1 2 3))
+(my-reverse2 (list 1 2 3))
+
